749978e478efaffd995be700523735b6688ebd58
Isabel Prado-Tucker <56853689+isabelpt@users.noreply.github.com>
2025-03-06 22:49:34 -0500

    Add files via upload

---
+++ blogPage.js
@@ -0,0 +1,22 @@
+console.log("HEYYYYYYY");
+
+var myIndex = 0;
+var x = document.getElementsByClassName("slideshow-images");
+carousel();
+
+// var x= document.getElementsByClassName("slideshow-images");
+
+function carousel(){
+  // console.log(x);
+
+  for (var i = 0; i<x.length; i++)
+  {
+    x[i].style.display="none";
+  }
+  myIndex++;
+ if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+
+
+}
